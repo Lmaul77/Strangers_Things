f777855cafec499602e30f34e9a30ab385f39831
Lucas <lmaul77@gmail.com>
2022-07-06 10:11:28 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import {App} from './components'
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world!</h1>);
+<BrowserRouter>
+<Routes>
+    <Route>
+        <App />
+    </Route>
+</Routes>
+</BrowserRouter>
