4a14b14f3b1a4305ba6a1fff715b7b52327c393e
Lucas <lmaul77@gmail.com>
2022-07-08 09:31:25 -0500

    morning debugging

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,132 @@
-import React, { useState, useEffect } from 'react';
-import {Route, Routes, Router, Navigate} from 'react-router-dom'
-import {Header, LoginLogout, Posts, Profile, Register, AddUserPosts, Home, Logout, EditUserPosts} from './';
+import React, { useState, useEffect } from "react";
+import { Route, Routes, Router, Navigate } from "react-router-dom";
+import {
+  Header,
+  LoginLogout,
+  Posts,
+  Profile,
+  Register,
+  AddUserPosts,
+  Home,
+  Logout,
+  EditUserPosts,
+} from "./";
 
-import './App.css'
+import "./App.css";
 
 const App = () => {
-    const [loggedIn, setLoggedIn] = useState(false)
-    const [titleInput, setTitleInput] =useState("")
-    const [descriptionInput, setDescriptionInput] = useState("")
-    const [priceInput, setPriceInput] = useState("")
-    const [authorInput, setAuthorInput] = useState("")
-    const [locationInput, setLocationInput] = useState("")
-    const [checkbox, setCheckbox] = useState("unchecked")
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [titleInput, setTitleInput] = useState("");
+  const [descriptionInput, setDescriptionInput] = useState("");
+  const [priceInput, setPriceInput] = useState("");
+  const [authorInput, setAuthorInput] = useState("");
+  const [locationInput, setLocationInput] = useState("");
+  const [checkbox, setCheckbox] = useState("unchecked");
 
-    return(
-        <div> 
-        <Header loggedIn={loggedIn}/>
-        <>
+  return (
+    <div>
+      <Header loggedIn={loggedIn} />
+      <>
         <Routes>
-            {loggedIn ?
+          {loggedIn ? (
             <>
-            <Route path="/posts" element={<Posts titleInput={titleInput} setTitleInput={setTitleInput} descriptionInput={descriptionInput} setDescriptionInput={setDescriptionInput} priceInput={priceInput} setPriceInput={setPriceInput} authorInput={authorInput} setAuthorInput={setAuthorInput} locationInput={locationInput} setLocationInput={setLocationInput} />} />
-            <Route path="/" element={<Home/>}/>
-            <Route path="/adduserposts" element={<AddUserPosts checkbox={checkbox} setCheckbox={setCheckbox} titleInput={titleInput} setTitleInput={setTitleInput} descriptionInput={descriptionInput} setDescriptionInput={setDescriptionInput} priceInput={priceInput} setPriceInput={setPriceInput} authorInput={authorInput} setAuthorInput={setAuthorInput} locationInput={locationInput} setLocationInput={setLocationInput}/>} />
-            <Route path="/edituserposts" element={<EditUserPosts checkbox={checkbox} setCheckbox={setCheckbox} titleInput={titleInput} setTitleInput={setTitleInput} descriptionInput={descriptionInput} setDescriptionInput={setDescriptionInput} priceInput={priceInput} setPriceInput={setPriceInput} authorInput={authorInput} setAuthorInput={setAuthorInput} locationInput={locationInput} setLocationInput={setLocationInput}/>} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/logout" element={<Logout setLoggedIn={setLoggedIn}/>}
-            />
+              <Route
+                path="/posts"
+                element={
+                  <Posts
+                    titleInput={titleInput}
+                    setTitleInput={setTitleInput}
+                    descriptionInput={descriptionInput}
+                    setDescriptionInput={setDescriptionInput}
+                    priceInput={priceInput}
+                    setPriceInput={setPriceInput}
+                    authorInput={authorInput}
+                    setAuthorInput={setAuthorInput}
+                    locationInput={locationInput}
+                    setLocationInput={setLocationInput}
+                  />
+                }
+              />
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/adduserposts"
+                element={
+                  <AddUserPosts
+                    checkbox={checkbox}
+                    setCheckbox={setCheckbox}
+                    titleInput={titleInput}
+                    setTitleInput={setTitleInput}
+                    descriptionInput={descriptionInput}
+                    setDescriptionInput={setDescriptionInput}
+                    priceInput={priceInput}
+                    setPriceInput={setPriceInput}
+                    authorInput={authorInput}
+                    setAuthorInput={setAuthorInput}
+                    locationInput={locationInput}
+                    setLocationInput={setLocationInput}
+                  />
+                }
+              />
+              <Route
+                path="/edituserposts"
+                element={
+                  <EditUserPosts
+                    checkbox={checkbox}
+                    setCheckbox={setCheckbox}
+                    titleInput={titleInput}
+                    setTitleInput={setTitleInput}
+                    descriptionInput={descriptionInput}
+                    setDescriptionInput={setDescriptionInput}
+                    priceInput={priceInput}
+                    setPriceInput={setPriceInput}
+                    authorInput={authorInput}
+                    setAuthorInput={setAuthorInput}
+                    locationInput={locationInput}
+                    setLocationInput={setLocationInput}
+                  />
+                }
+              />
+              <Route path="/profile" element={<Profile />} />
+              <Route
+                path="/logout"
+                element={<Logout setLoggedIn={setLoggedIn} />}
+              />
             </>
-            : 
+          ) : (
             <>
-            <Route path="/posts" element={<Posts titleInput={titleInput} setTitleInput={setTitleInput} descriptionInput={descriptionInput} setDescriptionInput={setDescriptionInput} priceInput={priceInput} setPriceInput={setPriceInput} authorInput={authorInput} setAuthorInput={setAuthorInput} locationInput={locationInput} setLocationInput={setLocationInput} />} />
-            <Route path="/register" element={<Register />} />    
-            <Route path="/login" element={<LoginLogout  loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
-            <Route path="/" element={<Home/>}/>
-            </>}
+              <Route
+                path="/posts"
+                element={
+                  <Posts
+                    checkbox={checkbox}
+                    setCheckbox={setCheckbox}
+                    titleInput={titleInput}
+                    setTitleInput={setTitleInput}
+                    descriptionInput={descriptionInput}
+                    setDescriptionInput={setDescriptionInput}
+                    priceInput={priceInput}
+                    setPriceInput={setPriceInput}
+                    authorInput={authorInput}
+                    setAuthorInput={setAuthorInput}
+                    locationInput={locationInput}
+                    setLocationInput={setLocationInput}
+                  />
+                }
+              />
+              <Route path="/register" element={<Register />} />
+              <Route
+                path="/login"
+                element={
+                  <LoginLogout loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+                }
+              />
+              <Route path="/" element={<Home />} />
+            </>
+          )}
         </Routes>
-        </>
-        </div>
-    )
-}
+      </>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import { editPost, GetPosts } from '../api/index.js'
-import { useNavigate } from "react-router-dom";
 import EditUserPosts from './EditUserPosts.js';
 
 // import './Posts.css'
 
 const Posts = ({checkbox, setCheckbox, titleInput, descriptionInput, priceInput, willDeliver, locationInput, setTitleInput, setDescriptionInput, setPriceInput, setLocationInput}) => {
 
-    const navigate = useNavigate()    
-
     function handleEdit () {
         const token = localStorage.getItem("token")
         editPost(token, titleInput, descriptionInput, priceInput, willDeliver, postId)
-        navigate("/edituserposts")
     }
 
     const [posts, setPosts] = useState([]);
@@ -34,7 +30,7 @@ const Posts = ({checkbox, setCheckbox, titleInput, descriptionInput, priceInput,
             <div id="Location">Location: { element.location }</div>
             <div id="WillDeliver">Willing to Deliver? { element.willDeliver ? "Yes" : "No" }</div>
             <EditUserPosts postId={postId} checkbox={checkbox} setCheckbox={setCheckbox} titleInput={titleInput} setTitleInput={setTitleInput} descriptionInput={descriptionInput} setDescriptionInput={setDescriptionInput} priceInput={priceInput} setPriceInput={setPriceInput} locationInput={locationInput} setLocationInput={setLocationInput}/>
-            <NavLink to="/edituserposts">Edit</NavLink>
+            <NavLink to="/edituserposts" onClick={handleEdit}>Edit</NavLink>
           </div>
             )
     })
